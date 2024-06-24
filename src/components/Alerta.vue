eb32fb9cdb4b9b54a1c021385e2ebefe2f97fb02
fullstackboss <romeltejada@gmail.com>
2024-06-24 08:42:34 -0500

    version con alerta pero sin separacion de estado

---
+++ src/components/Alerta.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  valerta: {
+    type: Object,
+    required: true,
+  },
+});
+const isError = computed(() => {
+  return props.valerta.tipo === "error";
+});
+</script>
+<template>
+  <div
+    :class="[isError ? 'bg-red-500' : 'bg-green-500']"
+    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md"
+  >
+    {{ valerta.mensaje }}
+  </div>
+</template>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { reactive } from 'vue'
+import Alerta from './Alerta.vue'
+
+const alerta = reactive({
+    tipo: '',
+    mensaje: ''
+})
+
 const paciente = reactive({
     nombre: '',
     propietario: '',
@@ -11,7 +18,8 @@ const paciente = reactive({
 const validar = () => {
     //console.log(Object.values(paciente));
     if (Object.values(paciente).includes('')) {
-        console.log('estan vacios')
+        alerta.mensaje = 'Todos los campos son obligatorios'
+        alerta.tipo = 'error'
         return
     }
 
@@ -27,46 +35,67 @@ const validar = () => {
         <p class="text-lg mt-5 text-center mb-10">
             Añade pacientes y <span class="text-indigo-600 font-bold">Administralos</span>
         </p>
-        <form class="bg-white shadow-xl rounded-lg py-6 px-5 mb-10" @submit.prevent="validar">
+        <Alerta v-if="alerta.mensaje" :valerta="alerta" />
+        <form 
+        class="bg-white shadow-xl rounded-lg py-6 px-5 mb-10" 
+        @submit.prevent="validar">
 
             <div class="mb-5">
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">
                     Nombre mascota - {{ paciente.nombre }}
                 </label>
-                <input type="text" id="mascota" class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
-                    placeholder="Nombre de la mascota" v-model="paciente.nombre">
+                <input 
+                    type="text" 
+                    id="mascota" 
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
+                    placeholder="Nombre de la mascota" 
+                    v-model="paciente.nombre">
             </div>
             <div class="mb-5">
                 <label for="propietario" class="block text-gray-700 uppercase font-bold">
                     Nombre propietario - {{ paciente.propietario }}
                 </label>
-                <input type="text" id="propietario" class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
-                    placeholder="Nombre del Propietario" v-model="paciente.propietario">
+                <input 
+                    type="text" 
+                    id="propietario" 
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
+                    placeholder="Nombre del Propietario" 
+                    v-model="paciente.propietario">
             </div>
             <div class="mb-5">
                 <label for="email" class="block text-gray-700 uppercase font-bold">
                     Email - {{ paciente.email }}
                 </label>
-                <input type="text" id="email" class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
-                    placeholder="Email del Propietario" v-model="paciente.email">
+                <input 
+                    type="text" 
+                    id="email" 
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
+                    placeholder="Email del Propietario" 
+                    v-model="paciente.email">
             </div>
             <div class="mb-5">
                 <label for="alta" class="block text-gray-700 uppercase font-bold">
                     Alta - {{ paciente.alta }}
                 </label>
-                <input type="date" id="alta" class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
+                <input 
+                    type="date" 
+                    id="alta" 
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md"
                     v-model="paciente.alta">
             </div>
-
             <div class="mb-5">
                 <label for="sintomas" class="block text-gray-700 uppercase font-bold">
                     Sintomas - {{ paciente.sintomas }}
                 </label>
-                <textarea id="sintomas" placeholder="Describir los sintomas"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md" v-model="paciente.sintomas">
+                <textarea 
+                    id="sintomas" 
+                    placeholder="Describir los sintomas"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-300 rounded-md" 
+                    v-model="paciente.sintomas">
                 </textarea>
             </div>
-            <input type="submit"
+            <input 
+                type="submit"
                 class="bg-indigo-600 w-full p-3 text-white uppercase rounded-full font-bold hover:bg-indigo-800 cursor-pointer transition-colors">
             </input>
         </form>
